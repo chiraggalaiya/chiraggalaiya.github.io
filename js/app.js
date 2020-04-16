e703419101a284712a9c43833efdac144410f563
Chirag Galaiya <chiraggalaiya@outlook.com>
2020-04-16 02:46:35 +0100

    update

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@ if ('serviceWorker' in navigator) {
   console.log('CLIENT: service worker is not supported.');
 }
 
+function isBetween(n, a, b) {
+   return (n - a) * (n - b) <= 0
+}
+
 var $ = Dom7;
 
 var routes = [
@@ -34,38 +38,76 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main');
 
-var colours = {"red": "#ff3b30", "green": "#4cd964", "blue": "#2196f3", "pink": "#ff2d55", "yellow": "#ffcc00", "orange": "#ff9500", "purple": "#9c27b0", "deeppurple": "#673ab7", "lightblue": "#5ac8fa", "teal": "#009688", "lime": "#cddc39", "deeporange": "#ff6b22"}
-
 window.onload = function() {
-  try { if (window.navigator.canShare(shareData)) { document.getElementById("share").style.display = "flex"; } } catch(e) { }
+  try { if (window.navigator.canShare({title:'',text:'',url:''})) { document.getElementById("share").style.display = "flex"; } } catch(e) { }
+  
   if (window.localStorage.getItem("username") == null) {
     window.localStorage.setItem("username", "");
   }
   document.getElementById("username").value = window.localStorage.getItem("username");
+  
   if (window.localStorage.getItem("theme-color") == null) {
     window.localStorage.setItem("theme-color", "pink");
+  } else {
+    document.documentElement.classList.replace("color-theme-pink", "color-theme-" + window.localStorage.getItem("theme-color"));
   }
-  document.documentElement.classList.replace("color-theme-pink", "color-theme-" + window.localStorage.getItem("theme-color"));
-  document.querySelector("meta[name=theme-color]").setAttribute("content", colours[window.localStorage.getItem("theme-color")]);
 }
 
 function update_colour(c) {
   document.documentElement.classList.replace("color-theme-"+window.localStorage.getItem("theme-color"), "color-theme-"+c);
   window.localStorage.setItem("theme-color", c);
   app.popover.close(document.getElementsByClassName("popover-colours")[0], true);
-  document.querySelector("meta[name=theme-color]").setAttribute("content", colours[window.localStorage.getItem("theme-color")]);
 }
 
+var person_popover = app.popover.get($('.popover-person')[0]);
 function person() {
   person_popover = app.popover.open(document.getElementsByClassName("popover-person")[0], document.getElementById("person_btn"), true);
-  if (document.getElementsByClassName("popover-person")[0].getElementsByTagName("input")[0].value == "") {
-    app.input.focus(document.getElementsByClassName("popover-person")[0].getElementsByTagName("input")[0]);
-    document.getElementsByClassName("popover-person")[0].getElementsByTagName("input")[0].focus();
+  if (person_popover.$el[0].getElementsByTagName("input")[0].value == "") {
+    app.input.focus(person_popover.$el[0].getElementsByTagName("input")[0]);
+    person_popover.$el[0].getElementsByTagName("input")[0].focus();
+  }
+}
+function update_username() {
+  var username = document.getElementById("username").value;
+  if (window.localStorage.getItem("username") != username) {
+    var new_username = "";
+    for (var i = 0; i < username.length; i++) {
+      if (isBetween(username.charCodeAt(i), 48, 57) || isBetween(username.charCodeAt(i), 65, 90) || isBetween(username.charCodeAt(i), 97, 122)) {
+        new_username += username[i];
+      }
+    }
+    if (new_username != "" && new_username.length > 3) {
+      if (username != new_username) { document.getElementById("username").value = new_username; }
+      window.localStorage.setItem("username", new_username);
+      app.toast.create({
+        text: 'Username updated to <b>'+new_username+'</b>',
+        closeTimeout: 1200,
+      }).open();
+      person_popover.close();
+    } else if (new_username != "" && new_username.length < 4) {
+      app.toast.create({
+        text: 'Username too short',
+        closeTimeout: 2000,
+      }).open();
+      app.input.focus(person_popover.$el[0].getElementsByTagName("input")[0]);
+      person_popover.$el[0].getElementsByTagName("input")[0].focus();
+    }
+  }
+}
+$('.popover-person').on('popover:closed', function (e) {
+  $('#username')[0].value = window.localStorage.getItem("username");
+});
+function check_username() {
+  if (window.localStorage.getItem("username") == "") {
+    app.toast.create({
+      text: 'Username required',
+      closeTimeout: 2000,
+    }).open();
+    $("#person_btn")[0].click();
+    return false;
+  } else {
+    return true;
   }
 }
 
-function share() {
-  if (usingCordova) {
-    window.plugins.socialsharing.shareWithOptions(shareOptions,function(){},function(){});
-  } else { window.navigator.share(shareData); }
-}
+function share() { window.navigator.share({title: 'Bukhar App by Chirag Galaiya', text: 'Bukhar App', url: "https://chilichingching.github.io"}); }
